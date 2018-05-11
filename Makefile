9a8d85fcea794279dc1dac897344124d6d515cfa
vladpurga <vladpurga@my.com>
2018-05-11 12:32:24 +0300

    v0.0.9 - brain-gcd

--- Makefile
+++ Makefile
@@ -10,6 +10,9 @@ brain-even:
 brain-calc:
 	npm run babel-node -- src/bin/brain-calc.js
 
+brain-gcd:
+	npm run babel-node -- src/bin/brain-gcd.js
+
 publish:
 	npm publish
 
---
+++ src/games/gcd.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import { getRandomInt, checkAnswers } from '../utils';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (a, b) => {
+  if (a === 0) {
+    return 1;
+  } else if (b === 0) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+
+const game = () => {
+  const countQuestions = 3;
+  for (let i = 0; i < countQuestions; i += 1) {
+    const number1 = getRandomInt(100);
+    const number2 = getRandomInt(100);
+    console.log(`Question: ${number1} ${number2}`);
+    const answer = readlineSync.question('Your answer? ');
+    const correctAnswer = gcd(number1, number2);
+
+    if (!checkAnswers(Number(answer), correctAnswer)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export { game, description };
